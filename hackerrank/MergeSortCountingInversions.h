8dd22e399b338f7d7d5a2c79ff5d208fcfeeafde
oleksiidiubankov <oleksii.diubankov@gmail.com>
2017-05-07 20:57:12 +0200

    Count inversions optimization

--- hackerrank/MergeSortCountingInversions.h
+++ hackerrank/MergeSortCountingInversions.h
@@ -2,35 +2,41 @@
 #define LEETCODESOLUTIONS_MERGESORTCOUNTINGINVERSIONS_H
 
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 namespace hackerrank
 {
-long long countInversionsImpl(vector<int>& a)
+long long countInversionsImpl(const vector<int>::iterator& aBegin, const vector<int>::iterator& aEnd)
 {
-    if (a.size() < 2)
+    auto arraySize = distance(aBegin, aEnd);
+    if (arraySize < 2)
         return 0;
 
-    vector<int> arr1(begin(a), begin(a) + a.size() / 2);
-    vector<int> arr2(begin(a) + a.size() / 2, end(a));
-    auto invCnt = countInversionsImpl(arr1) + countInversionsImpl(arr2);
-    for (auto arr1It = begin(arr1); arr1It != end(arr1); ++arr1It)
+    auto a1Begin = aBegin;
+    auto a1End = aBegin + arraySize / 2;
+    auto a2Begin = a1End;
+    auto a2End = aEnd;
+    auto invCnt = countInversionsImpl(a1Begin, a1End) + countInversionsImpl(a2Begin, a2End);
+    auto lastArr2It = a2Begin;
+    for (auto arr1It = a1Begin; arr1It != a1End; ++arr1It)
     {
-        auto arr2It = lower_bound(begin(arr2), end(arr2), *arr1It);
-        invCnt += distance(begin(arr2), arr2It);
+        auto arr2It = lower_bound(lastArr2It, a2End, *arr1It);
+        invCnt += distance(a2Begin, arr2It);
+        lastArr2It = arr2It;
     }
 
     vector<int> temp;
-    temp.reserve(a.size());
-    std::merge(begin(arr1), end(arr1), begin(arr2), end(arr2), back_inserter(temp));
-    a = temp;
+    temp.reserve(arraySize);
+    merge(a1Begin, a1End, a2Begin, a2End, back_inserter(temp));
+    copy(begin(temp), end(temp), aBegin);
     return invCnt;
 }
 
 long long count_inversions(vector<int> a)
 {
-    return countInversionsImpl(a);
+    return countInversionsImpl(begin(a), end(a));
 }
 }
 
