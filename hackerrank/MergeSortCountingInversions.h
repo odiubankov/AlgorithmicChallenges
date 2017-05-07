e94e958b267c9b7a0826cc4df4f9b9fa8ee1905a
oleksiidiubankov <oleksii.diubankov@gmail.com>
2017-05-07 20:21:32 +0200

    Count inversions optimization

--- hackerrank/MergeSortCountingInversions.h
+++ hackerrank/MergeSortCountingInversions.h
@@ -7,47 +7,31 @@ using namespace std;
 
 namespace hackerrank
 {
-    long long countInversionsImpl(vector<int>::iterator aBegin, vector<int>::iterator aEnd)
+long long countInversionsImpl(vector<int>& a)
+{
+    if (a.size() < 2)
+        return 0;
+
+    vector<int> arr1(begin(a), begin(a) + a.size() / 2);
+    vector<int> arr2(begin(a) + a.size() / 2, end(a));
+    auto invCnt = countInversionsImpl(arr1) + countInversionsImpl(arr2);
+    for (auto arr1It = begin(arr1); arr1It != end(arr1); ++arr1It)
     {
-        auto arraySize = distance(aBegin, aEnd);
-        if (arraySize < 2)
-            return 0;
-
-        auto leftSubArrayItBegin = aBegin;
-        auto leftSubArrayItEnd = aBegin + arraySize / 2;
-        auto rightSubArrayItBegin = leftSubArrayItEnd + 1;
-        auto rightSubArrayItEnd = aEnd;
-        auto invCnt = countInversionsImpl(leftSubArrayItBegin, leftSubArrayItEnd) +
-            countInversionsImpl(rightSubArrayItBegin, rightSubArrayItEnd);
-
-        for (auto leftSubArrayIt = leftSubArrayItBegin;
-             leftSubArrayIt != leftSubArrayItEnd;
-             ++leftSubArrayIt)
-        {
-            if (*leftSubArrayIt > *rightSubArrayItBegin)
-            {
-                ++invCnt;
-                swap(*leftSubArrayIt, *rightSubArrayItBegin);
-                for (auto rightSubArrayIt = rightSubArrayItBegin;
-                     rightSubArrayIt != (rightSubArrayItEnd - 1);
-                     ++rightSubArrayIt)
-                {
-                    if (*rightSubArrayIt > *(rightSubArrayIt + 1))
-                    {
-                        swap(*rightSubArrayIt, *(rightSubArrayIt + 1));
-                        ++invCnt;
-                    }
-                }
-            }
-        }
-
-        return invCnt;
+        auto arr2It = lower_bound(begin(arr2), end(arr2), *arr1It);
+        invCnt += distance(begin(arr2), arr2It);
     }
 
-    long long count_inversions(vector<int> a)
-    {
-        return countInversionsImpl(begin(a), end(a));
-    }
+    vector<int> temp;
+    temp.reserve(a.size());
+    std::merge(begin(arr1), end(arr1), begin(arr2), end(arr2), back_inserter(temp));
+    a = temp;
+    return invCnt;
+}
+
+long long count_inversions(vector<int> a)
+{
+    return countInversionsImpl(a);
+}
 }
 
 #endif //LEETCODESOLUTIONS_MERGESORTCOUNTINGINVERSIONS_H
