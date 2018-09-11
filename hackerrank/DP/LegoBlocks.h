a62fc5a0323399b3cd0beeec28b3b4a92d3591d6
oleksiidiubankov <oleksii.diubankov@gmail.com>
2018-09-11 14:21:52 +0200

    lego blocks updated

--- hackerrank/DP/LegoBlocks.h
+++ hackerrank/DP/LegoBlocks.h
@@ -1,53 +1,100 @@
 #pragma once
 
+#include <unordered_map>
+
+using ULL = unsigned long long;
+
 namespace hackerrank {
 
-	long long getAllWallCnt(long long w, long long h) {
+    ULL getAllWallCnt(ULL w, ULL h) {
 	    if (w == 0)
             return 0;
 
+	    static std::unordered_map<ULL, std::unordered_map<ULL, ULL>> cache;
+	    auto cacheIt1 = cache.find(w);
+	    if (cacheIt1 != end(cache)){
+	        auto cacheIt2 = cacheIt1->second.find(h);
+	        if (cacheIt2 != std::end(cacheIt1->second))
+                return cacheIt2->second;
+	    }
+
 		if (h == 1) {
-			long long result = 0;
-			if (w > 4)
-				result += getAllWallCnt(w - 4, 1);
-			if (w > 3)
-				result += getAllWallCnt(w - 3, 1);
-			if (w > 2)
-				result += getAllWallCnt(w - 2, 1);
-			if (w > 1)
-				result += getAllWallCnt(w - 1, 1);
-			if (w == 1 || w == 2 || w == 3 || w == 4)
-				result += 1;
-			return result;
+            ULL result = 0;
+            const ULL MaxBlockLength = 4;
+			for (ULL i = 1; i <= MaxBlockLength; ++i) {
+			    if (w > i)
+                    result += getAllWallCnt(w - i, 1);
+			    else if (w == i)
+			        result += 1;
+			}
+
+			cache[w][h] = result;
+            return result;
 		}
 
 		auto height1Cnt = getAllWallCnt(w, 1);
-		long long result = height1Cnt;
-		for (int i = 1; i < h; ++i)
-			result *= height1Cnt;
+        ULL result = height1Cnt;
+		for (int i = 1; i < h; ++i) {
+            result *= (height1Cnt);
+		}
 
+        cache[w][h] = result;
         return result;
 	}
 
-	long long getStableWallCnt(long long w, long long h);
+    ULL getAllGapCnt(ULL w, ULL h);
 
-	long long getGapCnt(long long w, long long h) {
+    ULL getGapCnt(ULL w, ULL h, ULL n) {
+        if (w <= 1)
+            return 0;
+
+        static std::unordered_map<ULL,
+            std::unordered_map<ULL,
+                               std::unordered_map<ULL, ULL>>> cache;
+
+        auto cacheIt1 = cache.find(w);
+        if (cacheIt1 != end(cache)){
+            auto cacheIt2 = cacheIt1->second.find(h);
+            if (cacheIt2 != std::end(cacheIt1->second)){
+                auto cacheIt3 = cacheIt2->second.find(n);
+                if (cacheIt3 != std::end(cacheIt2->second))
+                    return cacheIt3->second;
+            }
+        }
+
+        ULL result = 0;
+        for (int i = 1; i <= w - 1; ++i) {
+            auto leftCnt = getAllWallCnt(i, h) - getAllGapCnt(i, h);
+            ULL rightCnt = 0;
+            if (n == 1)
+                rightCnt = getAllWallCnt(w - i, h) - getAllGapCnt(w - i, h);
+            else
+                rightCnt = getGapCnt(w - i, h, n - 1);
+
+            result += (leftCnt) * (rightCnt);
+        }
+
+        cache[w][h][n] = result;
+        return result;
+    }
+
+    ULL getAllGapCnt(ULL w, ULL h) {
 	    if (w <= 1)
             return 0;
 
-	    long long result = 0;
-	    for (int i = 1; i <= w - 1; ++i) {
-            auto leftCnt = getAllWallCnt(i, h) - getGapCnt(i, h);
-            auto rightCnt = getAllWallCnt(w - i, h) - getGapCnt(w - i, h);
-            result += leftCnt * rightCnt;
-        }
+        ULL result = 0;
+	    for (ULL i = 1; i <= w - 1; ++i) {
+            result += getGapCnt(w, h, i);
+	    }
 
         return result;
 	}
 
-	long long getStableWallCnt(long long w, long long h) {
+    ULL getStableWallCnt(ULL w, ULL h) {
 		auto result = getAllWallCnt(w, h);
-		result -= getGapCnt(w, h);
+		auto gapCnt = getAllGapCnt(w, h);
+		result -= gapCnt;
+		result %= 1000000007;
         return result;
 	}
 
--- hackerrank/DP/tests/LegoBlocks.cpp
+++ hackerrank/DP/tests/LegoBlocks.cpp
@@ -15,5 +15,25 @@ TEST(LegoBlocks, test3) {
     ASSERT_EQ(getStableWallCnt(3, 2), 9);
 }
 
+TEST(LegoBlocks, test4) {
+    ASSERT_EQ(getStableWallCnt(4, 4), 3375);
+}
+
+TEST(LegoBlocks, test5) {
+    ASSERT_EQ(getStableWallCnt(3, 7), 16129);
+}
+
+TEST(LegoBlocks, test6) {
+    ASSERT_EQ(getStableWallCnt(8, 3), 328650);
+}
+
+TEST(LegoBlocks, test7) {
+    ASSERT_EQ(getStableWallCnt(7, 4), 5562914);
+}
+
+TEST(LegoBlocks, test8) {
+    ASSERT_EQ(getStableWallCnt(6, 8), 402844528);
+}
+
 }
 
