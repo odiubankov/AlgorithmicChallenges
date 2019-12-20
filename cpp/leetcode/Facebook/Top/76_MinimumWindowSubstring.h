89de2f2793c4cc327f812b421aaa6d78eb82ecc9
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2019-12-20 18:27:04 +0100

    leetcode: cleanup of min window substring

--- cpp/leetcode/Facebook/Top/76_MinimumWindowSubstring.h
+++ cpp/leetcode/Facebook/Top/76_MinimumWindowSubstring.h
@@ -7,67 +7,62 @@
 #include <algorithm>
 using namespace std;
 
+using ChCntT = unordered_map<char, int>;
+using StrItT = string::const_iterator;
+
+ChCntT calcChCnt(const string& str) {
+    ChCntT chCnt;
+    for (char c : str)
+        ++chCnt[c];
+    return chCnt;
+}
+
+StrItT getWindowEnd(StrItT strIt, StrItT strEnd, ChCntT& chCnt, size_t numToFind) {
+    for (; strIt != strEnd; ++strIt) {
+        auto chIt = chCnt.find(*strIt);
+        if (chIt != end(chCnt)) {
+            if (--(chIt->second) >= 0) {
+                if (--numToFind == 0)
+                    return strIt;
+            }
+        }
+    }
+    return strEnd;
+}
+
+StrItT getWindowBegin(StrItT it, ChCntT& chCnt) {
+    for (;;++it) {
+        auto chIt = chCnt.find(*it);
+        if (chIt != end(chCnt)) {
+            if (chIt->second >= 0)
+                return it;
+            ++(chIt->second);
+        }
+    }
+}
+
 string minWindow(const string& str1, const string& str2) {
     if (str2.empty())
         return "";
-    unordered_map<char, int> chCnt;
-    for (char c : str2)
-        ++chCnt[c];
-    auto inStr2 = [&chCnt](auto c){ return chCnt.find(c) != end(chCnt); };
-    auto chIt = find_if(begin(str1), end(str1), inStr2);
-    if (chIt == end(str1))
+    auto chCnt = calcChCnt(str2);
+    auto windowEnd = getWindowEnd(begin(str1), end(str1), chCnt, str2.length());
+    if (windowEnd == end(str1))
         return "";
-    auto toFind = str2.size() - 1;
-    --chCnt[*chIt];
-    auto sB = chIt;
-    ++chIt;
-    while (toFind != 0 && chIt != end(str1)) {
-        chIt = find_if(chIt, end(str1), inStr2);
-        if (chIt == end(str1))
-            return "";
-        if (--chCnt[*chIt] >= 0)
-            --toFind;
-        ++chIt;
-    }
-    if (toFind != 0)
-        return "";
-    auto resSize = distance(sB, chIt);
-    auto resB = sB;
-    do {
-        bool needFind = false;
-        char c;
-        if (++chCnt[*sB] == 1) {
-            needFind = true;
-            c = *sB;
-        }
-        ++sB;
-        for (;sB != end(str1); ++sB) {
-            auto chCntIt = chCnt.find(*sB);
-            if (chCntIt != end(chCnt)){
-                if (chCntIt->second >= 0)
-                    break;
-                ++chCntIt->second;
-            }
-        }
-
-        while (chIt != end(str1) && needFind) {
-            chIt = find_if(chIt, end(str1), inStr2);
-            if (chIt == end(str1))
-                break;
-            --chCnt[*chIt];
-            if (*chIt == c)
-                needFind = false;
-            ++chIt;
-        }
-        if (needFind)
+    auto windowBegin = getWindowBegin(begin(str1), chCnt);
+    auto minWindowBegin = windowBegin;
+    auto minWindowEnd = windowEnd;
+    for(;;) {
+        ++chCnt[*windowBegin];
+        windowEnd = getWindowEnd(windowEnd + 1, end(str1), chCnt, 1);
+        if (windowEnd == end(str1))
             break;
-        auto newSize = distance(sB, chIt);
-        if (newSize < resSize) {
-            resSize = newSize;
-            resB = sB;
+        windowBegin = getWindowBegin(windowBegin + 1, chCnt);
+        if (distance(windowBegin, windowEnd) < distance(minWindowBegin, minWindowEnd)) {
+            minWindowBegin = windowBegin;
+            minWindowEnd = windowEnd;
         }
-    } while (resSize > str2.size());
-    return string{resB, resB + resSize};
+    }
+    return string(minWindowBegin, minWindowEnd + 1);
 }
 
 #endif //ALGORITHMICCHALLENGES_76_MINIMUMWINDOWSUBSTRING_H
