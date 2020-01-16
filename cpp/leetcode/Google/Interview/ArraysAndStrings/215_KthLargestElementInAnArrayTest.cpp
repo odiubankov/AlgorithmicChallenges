f208a6991c6f54c228663eba41712164cdf71db9
oleksiidiubankov <oleksii.diubankov@gmail.com>
2020-01-16 21:37:17 +0100

    leetcode: add test for kth largest element

---
+++ cpp/leetcode/Google/Interview/ArraysAndStrings/215_KthLargestElementInAnArrayTest.cpp
@@ -0,0 +1,7 @@
+#include "215_KthLargestElementInAnArray.h"
+#include <gtest/gtest.h>
+
+TEST(KthLargestElementInAnArray, test) {
+    vector<int> vals{3,2,1,5,6,4};
+    ASSERT_EQ(5,findKthLargestHeap(vals, 2));
+}
