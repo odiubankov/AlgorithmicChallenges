113ca9c0be7b0395b78de6ea10f1b5d8ae8e9359
oleksiidiubankov <oleksii.diubankov@gmail.com>
2020-01-16 21:35:54 +0100

    leetcode: simplify license key formatting

--- cpp/leetcode/LicenseKeyFormatting.h
+++ cpp/leetcode/LicenseKeyFormatting.h
@@ -21,53 +21,43 @@
 
 #include <string>
 #include <algorithm>
+#include <sstream>
+using namespace std;
 
 namespace leetcode
 {
 
-inline constexpr char toUpperDiff() noexcept
-{
+inline constexpr char toUpperDiff() noexcept {
     return 'A' - 'a';
 }
 
-inline constexpr bool isLowerCase(char character) noexcept
-{
+inline constexpr bool isLowerCase(char character) noexcept {
     return character >= 'a' && character <= 'z';
 }
 
 class Solution
 {
 public:
-    std::string licenseKeyFormatting(std::string S, int K)
-    {
-        std::string result;
+    const char Separator = '-';
+    char toUpper(char c) {
+        return isLowerCase(c) ? c + toUpperDiff() : c;
+    }
+    string licenseKeyFormatting(const string& S, const int K) {
+        stringstream ss;
         int currentGroupCnt = 0;
-        auto i = S.size();
-        do {
-            --i;
-            if (currentGroupCnt == K) {
-                result.push_back('-');
-                currentGroupCnt = 0;
+        for (auto sIt = rbegin(S); sIt != rend(S); ++sIt) {
+            if (*sIt != Separator) {
+                if (currentGroupCnt == K) {
+                    ss << Separator;
+                    currentGroupCnt = 0;
+                }
+                ss << toUpper(*sIt);
+                ++currentGroupCnt;
             }
-
-            if (S[i] == '-') {
-                continue;
-            }
-
-            if (isLowerCase(S[i])) {
-                result.push_back(S[i] + toUpperDiff());
-            }
-            else {
-                result.push_back(S[i]);
-            }
-
-            ++currentGroupCnt;
         }
-        while (i > 0);
-
-        result.erase(result.find_last_not_of('-') + 1);
-        reverse(begin(result), end(result));
-        return result;
+        auto res = ss.str();
+        reverse(begin(res), end(res));
+        return res;
     }
 };
 
