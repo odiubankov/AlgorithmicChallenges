0928dddb54f5c1e728cc5e41f7c73e8da2f35e81
oleksiidiubankov <oleksii.diubankov@gmail.com>
2020-01-19 10:01:55 +0100

    leetcode: coins change optimization

--- cpp/leetcode/Google/Interview/DP/322_CoinChange.h
+++ cpp/leetcode/Google/Interview/DP/322_CoinChange.h
@@ -6,9 +6,10 @@
 #include <limits>
 using namespace std;
 
-int coinChange(const vector<int>& coins, int amount) {
+int coinChange(vector<int>& coins, int amount) {
     if (amount == 0)
         return 0;
+    sort(begin(coins), end(coins));
     vector<int> amountCoinsCnt(amount + 1, numeric_limits<int>::max());
     amountCoinsCnt[0] = 0;
     for (int i = 1; i != amountCoinsCnt.size(); ++i) {
@@ -16,6 +17,8 @@ int coinChange(const vector<int>& coins, int amount) {
         for (auto coin : coins) {
             if (i >= coin)
                 minCoinsCnt = min(amountCoinsCnt[i - coin], minCoinsCnt);
+            else
+                break;
         }
         if (minCoinsCnt != numeric_limits<int>::max())
             ++minCoinsCnt;
