1833c8656854ca32d967c442a79626cf0768873e
Oleksii Diubankov <Oleksii.Diubankov@bmw.de>
2020-01-20 17:40:53 +0100

    leetcode: use priority queue with network delay time calculation

--- cpp/leetcode/Google/Top/743_NetworkDelayTime.h
+++ cpp/leetcode/Google/Top/743_NetworkDelayTime.h
@@ -4,26 +4,36 @@
 
 #include <vector>
 #include <set>
+#include <queue>
+#include <algorithm>
 using namespace std;
+
+using NodeDistanceT = pair<int, int>;
+struct NodeDistanceComp {
+    bool operator()(const NodeDistanceT& node1, const NodeDistanceT& node2) const {
+        return node1.first > node2.first;
+    }
+};
+
+using NodesQueueT = priority_queue<NodeDistanceT, vector<NodeDistanceT>, NodeDistanceComp>;
 
 int networkDelayTime(const vector<vector<int>>& times, int N, int K) {
     vector<vector<pair<int , int>>> connections(N);
     for (auto& time : times)
         connections[time[0] - 1].push_back(make_pair(time[1] - 1, time[2]));
-    multimap<int, int> toVisit;
-    toVisit.emplace(0, K - 1);
+    NodesQueueT nodesQueue;
+    nodesQueue.emplace(0, K - 1);
     vector<bool> visited(N, false);
     int maxDistance = 0;
-    while (!toVisit.empty()) {
-        auto nextIt = begin(toVisit);
-        auto next = *nextIt;
-        toVisit.erase(nextIt);
+    while (!nodesQueue.empty()) {
+        auto next = nodesQueue.top();
+        nodesQueue.pop();
         if (visited[next.second])
             continue;
         visited[next.second] = true;
         maxDistance = max(maxDistance, next.first);
         for (const auto& connection : connections[next.second])
-            toVisit.emplace(next.first + connection.second, connection.first);
+            nodesQueue.emplace(next.first + connection.second, connection.first);
     }
     return all_of(begin(visited), end(visited), [](auto item) { return item; }) ? maxDistance : -1;
 }
