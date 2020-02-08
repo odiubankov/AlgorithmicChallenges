a628e6fcbbbd8aac2ed43c479d121622fe3f0091
oleksiidiubankov <oleksii.diubankov@gmail.com>
2020-02-08 21:10:39 +0100

    leetcode: word squares optimization

--- cpp/leetcode/Google/Interview/Recursion/425_WordSquares.h
+++ cpp/leetcode/Google/Interview/Recursion/425_WordSquares.h
@@ -7,60 +7,21 @@
 #include <array>
 #include <memory>
 #include <unordered_set>
+
 using namespace std;
 
 namespace WordSquares {
 
-constexpr size_t getAlphabetSize() { return 'z' - 'a' + 1; }
-constexpr size_t getCharIndex(char c) { return c - 'a'; }
-constexpr char getChar(size_t index) { return 'a' + index; }
+inline constexpr size_t getAlphabetSize() { return 'z' - 'a' + 1; }
+inline constexpr size_t getCharIndex(char c) { return c - 'a'; }
+inline constexpr char getChar(size_t index) { return 'a' + index; }
 
 struct Trie {
-    array<shared_ptr<Trie>, getAlphabetSize()> children_;
-    unordered_set<size_t> liveChildren_;
+    Trie(size_t val) : val_{val} {}
+    array<unique_ptr<Trie>, getAlphabetSize()> children_;
+    vector<size_t> nextChars_;
+    size_t val_ = 0;
 };
-
-vector<string> getAllPostfixes(const Trie *trie) {
-    if (trie->liveChildren_.empty())
-        return {""};
-    vector<string> res;
-    for (auto child : trie->liveChildren_) {
-        char c = getChar(child);
-        for (const auto& postfixStr : getAllPostfixes(trie->children_[child].get())) {
-            res.push_back(c + postfixStr);
-        }
-    }
-    return res;
-}
-
-vector<vector<string>> getWordSquares(const vector<string>& words, const Trie &trie) {
-    if (!words.empty() && words.size() == words.front().size())
-        return {words};
-    const Trie* triePrefix = &trie;
-    string prefixStr;
-    for (size_t i = 0; i < words.size(); ++i) {
-        auto nextChar = words[i][words.size()];
-        auto index = getCharIndex(nextChar);
-        if (!triePrefix->children_[index]) {
-            triePrefix = nullptr;
-            break;
-        }
-        else {
-            prefixStr += nextChar;
-            triePrefix = triePrefix->children_[index].get();
-        }
-    }
-    vector<vector<string>> res;
-    if (triePrefix) {
-        for (const auto &postfixStr : getAllPostfixes(triePrefix)) {
-            auto nextWords = words;
-            nextWords.push_back(prefixStr + postfixStr);
-            auto wordRes = getWordSquares(nextWords, trie);
-            copy(begin(wordRes), end(wordRes), back_inserter(res));
-        }
-    }
-    return res;
-}
 
 void fillTrie(const vector<string> &words, Trie& trie) {
     for (const auto& word : words) {
@@ -68,18 +29,105 @@ void fillTrie(const vector<string> &words, Trie& trie) {
         for (auto c : word) {
             size_t cI = getCharIndex(c);
             if (!triePrefix->children_[cI]) {
-                triePrefix->children_[cI] = make_shared<Trie>();
-                triePrefix->liveChildren_.insert(cI);
+                triePrefix->children_[cI] = make_unique<Trie>(cI);
+                triePrefix->nextChars_.push_back(cI);
             }
             triePrefix = triePrefix->children_[cI].get();
         }
     }
 }
 
+bool isDiagnoal(size_t row, size_t col) {
+    return row == col;
+}
+
+bool isLastCell(
+    size_t row, size_t col,
+    size_t lastRow, size_t lastCol) {
+    return row == lastRow && col == lastCol;
+}
+
+struct Cell {
+    explicit Cell(Trie* row = nullptr, Trie* col = nullptr) : row_{row}, col_{col} {}
+    Trie* row_;
+    Trie* col_;
+};
+
+Cell getPrevColCell(
+    size_t row, size_t col,
+    const vector<vector<Cell>>& square) {
+    return square[row][col - 1];
+}
+
+Cell getPrevRowCell(
+    size_t row, size_t col,
+    const vector<vector<Cell>>& square) {
+    return square[row - 1][col];
+}
+
+void addSquare(const vector<vector<Cell>>& square,
+               vector<vector<string>>& resSquares) {
+    vector<string> wordSquare;
+    wordSquare.reserve(square.size() - 1);
+    for (size_t wordI = 1; wordI != square.size(); ++wordI) {
+        string word;
+        word.reserve(square.size() - 1);
+        size_t wordRow = wordI, wordCol = wordI;
+        for (size_t i = 1; i <= wordCol; ++i)
+            word.push_back(getChar(square[wordRow][i].row_->val_));
+        for (size_t i = wordRow + 1; i != square.size(); ++i)
+            word.push_back(getChar(square[i][wordCol].row_->val_));
+        wordSquare.push_back(move(word));
+    }
+    resSquares.push_back(move(wordSquare));
+}
+
+void fillCell(size_t row, size_t col,
+              vector<vector<Cell>>& square,
+              vector<vector<string>>& resSquares) {
+    if (isDiagnoal(row, col)) {
+        auto prevColCell = getPrevColCell(row, col, square);
+        auto lastCell = isLastCell(row, col,
+                                   square.size() - 1, square.front().size() - 1);
+        for (auto nextChar : prevColCell.row_->nextChars_) {
+            square[row][col].row_ = prevColCell.row_->children_[nextChar].get();
+            square[row][col].col_ = prevColCell.row_->children_[nextChar].get();
+            if (lastCell) {
+                addSquare(square, resSquares);
+            } else {
+                fillCell(row + 1, 1, square, resSquares);
+            }
+        }
+    } else {
+        auto prevColCell = getPrevColCell(row, col, square);
+        auto prevRowCell = getPrevRowCell(row, col, square);
+        for (auto prevColNextChar : prevColCell.row_->nextChars_) {
+            if (prevRowCell.col_->children_[prevColNextChar]) {
+                square[row][col].row_ = prevColCell.row_->children_[prevColNextChar].get();
+                square[row][col].col_ = prevRowCell.col_->children_[prevColNextChar].get();
+                fillCell(row, col + 1, square, resSquares);
+            }
+        }
+    }
+}
+
 vector<vector<string>> wordSquares(const vector<string> &words) {
-    Trie trie;
+    if (words.empty() || words.front().empty())
+        return {};
+    Trie trie {0};
     fillTrie(words, trie);
-    return getWordSquares({}, trie);
+    vector<vector<Cell>> square(
+        words.front().size() + 1,
+        vector<Cell>(words.front().size() + 1));
+    for (auto& row : square) {
+        row[0].row_ = &trie;
+        row[0].col_ = &trie;
+    }
+    square.front()[1].col_ = &trie;
+    square.front()[1].row_ = &trie;
+    vector<vector<string>> resSquares;
+    fillCell(1, 1, square, resSquares);
+    return resSquares;
 }
 }
 
--- cpp/leetcode/Google/Interview/Recursion/425_WordSquaresTest.cpp
+++ cpp/leetcode/Google/Interview/Recursion/425_WordSquaresTest.cpp
@@ -4,3 +4,30 @@
 TEST(WordSquares, test) {
     ASSERT_EQ(2, WordSquares::wordSquares({"area","lead","wall","lady","ball"}).size());
 }
+
+TEST(WordSquares, test2) {
+    ASSERT_EQ(1, WordSquares::wordSquares({"a"}).size());
+}
+
+TEST(WordSquares, test3) {
+    ASSERT_EQ(1, WordSquares::wordSquares({"ab", "bc"}).size());
+}
+
+TEST(WordSquares, test4) {
+    ASSERT_EQ(2, WordSquares::wordSquares({"ab", "bc", "bd"}).size());
+}
+
+TEST(WordSquares, test5) {
+    ASSERT_EQ(2, WordSquares::wordSquares({"ab", "cb", "bd"}).size());
+}
+
+TEST(WordSquares, test6) {
+    ASSERT_EQ(4, WordSquares::wordSquares({"ab", "cb", "bd", "be"}).size());
+}
+//abc
+//bde
+//cef
+
+TEST(WordSquares, test7) {
+    ASSERT_EQ(1, WordSquares::wordSquares({"abc", "bde", "cef"}).size());
+}
