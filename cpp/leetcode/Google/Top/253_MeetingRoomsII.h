a7d23b99fb6694d269f2b3d038d2fa98be4f340b
oleksiidiubankov <oleksii.diubankov@gmail.com>
2020-02-12 22:35:42 +0100

    leetcode: meeting room priority queue

--- cpp/leetcode/Google/Top/253_MeetingRoomsII.h
+++ cpp/leetcode/Google/Top/253_MeetingRoomsII.h
@@ -4,23 +4,26 @@
 
 #include <vector>
 #include <algorithm>
+#include <queue>
 
-int minMeetingRooms(std::vector<std::vector<int>>& intervals) {
+using namespace std;
+
+int minMeetingRooms(vector<vector<int>>& intervals) {
     if (intervals.empty())
         return 0;
-    std::sort(begin(intervals), end(intervals),
+    sort(begin(intervals), end(intervals),
         [](const auto& a, const auto& b) { return a.front() < b.front(); });
-    std::vector<int> rooms(intervals.size(), 1);
-    for (std::size_t i = 0; i < intervals.size(); ++i) {
+    vector<int> rooms(intervals.size(), 1);
+    for (size_t i = 0; i < intervals.size(); ++i) {
         for (auto j = i + 1; j < intervals.size() && intervals[j].front() < intervals[i].back(); ++j) {
             ++rooms[j];
         }
     }
-    return *std::max_element(begin(rooms), end(rooms));
+    return *max_element(begin(rooms), end(rooms));
 }
 
 int minMeetingRoomsSimple(std::vector<std::vector<int>>& intervals) {
-    std::vector<int> begins, ends;
+    vector<int> begins, ends;
     begins.reserve(intervals.size());
     ends.reserve(intervals.size());
     for (const auto& interval : intervals) {
@@ -29,7 +32,7 @@ int minMeetingRoomsSimple(std::vector<std::vector<int>>& intervals) {
     }
     sort(begin(begins), end(begins));
     sort(begin(ends), end(ends));
-    std::size_t beginIndex = 0, endIndex = 0;
+    size_t beginIndex = 0, endIndex = 0;
     int rooms = 0, minRooms = 0;
     while (beginIndex < begins.size()) {
         if (begins[beginIndex] < ends[endIndex]) {
@@ -40,6 +43,27 @@ int minMeetingRoomsSimple(std::vector<std::vector<int>>& intervals) {
         } else {
             --rooms;
             ++endIndex;
+        }
+    }
+    return minRooms;
+}
+
+int minMeetingRoomsPriorityQueue(vector<std::vector<int>>& intervals) {
+    sort(begin(intervals), end(intervals),
+        [](const auto& interval1, const auto& interval2) {
+            return interval1.front() < interval2.front();
+    });
+    priority_queue<int, vector<int>, greater<>> intervalEnds;
+    int rooms = 0, minRooms = 0;
+    for (auto intervalIt = begin(intervals); intervalIt != end(intervals);) {
+        if (intervalEnds.empty() || intervalIt->front() < intervalEnds.top()) {
+            ++rooms;
+            minRooms = max(minRooms, rooms);
+            intervalEnds.push(intervalIt->back());
+            ++intervalIt;
+        } else {
+            --rooms;
+            intervalEnds.pop();
         }
     }
     return minRooms;
