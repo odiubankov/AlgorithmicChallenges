51dac587f5060d27ca803f8d2020ef62dd46032a
oleksiidiubankov <oleksii.diubankov@gmail.com>
2020-03-16 20:43:08 +0100

    leetcode: improve minimum domino rotations

--- cpp/leetcode/Google/Top/1007_MinimumDominoRotations.h
+++ cpp/leetcode/Google/Top/1007_MinimumDominoRotations.h
@@ -5,47 +5,44 @@
 #include <vector>
 #include <limits>
 
-void getMinRotations(int& minRotations, int rotations) {
-    if (rotations != -1 && rotations < minRotations)
-        minRotations = rotations;
+int getMinRotationsBottom(const std::vector<int>& top, const std::vector<int>& bottom) {
+    if (top.empty())
+        return 0;
+    int minRotations = 0;
+    auto val = top.front();
+    for (auto topIt = begin(top), bottomIt = begin(bottom); topIt != end(top); ++topIt, ++bottomIt) {
+        if (*bottomIt == val)
+            continue;
+        if (*topIt == val)
+            ++minRotations;
+        else
+            return std::numeric_limits<int>::max();
+    }
+    return minRotations;
 }
 
-bool updateRotations(int val, int& valTop, int& valBottom, int top, int bottom, int other) {
-    if (valTop != -1) {
-        if (val != top && val != bottom) {
-            if (other == -1)
-                return false;
-            valTop = -1;
-            valBottom = -1;
-        } else if (val == top && val != bottom) {
-            ++valBottom;
-        } else if (val != top && val == bottom) {
-            ++valTop;
-        }
+int getMinRotationsTop(const std::vector<int>& top, const std::vector<int>& bottom) {
+    if (top.empty())
+        return 0;
+    int minRotations = 0;
+    auto val = top.front();
+    for (auto topIt = begin(top), bottomIt = begin(bottom); topIt != end(top); ++topIt, ++bottomIt) {
+        if (*topIt == val)
+            continue;
+        if (*bottomIt == val)
+            ++minRotations;
+        else
+            return std::numeric_limits<int>::max();
     }
-    return true;
+    return minRotations;
 }
 
 int minDominoRotations(const std::vector<int>& A, const std::vector<int>& B) {
-    if (A.empty())
-        return 0;
-    auto Ait = begin(A);
-    auto Bit = begin(B);
-    int val1 = *Ait;
-    int val2 = *Bit;
-    int val1Top = 0, val2Top = val2 != val1 ? 1 : 0, val1Bottom = val2 != val1 ? 1 : 0, val2Bottom = 0;
-    ++Ait;
-    ++Bit;
-    for (;Ait != end(A); ++Ait, ++Bit) {
-        if (!updateRotations(val1, val1Top, val1Bottom, *Ait, *Bit, val2Top) ||
-            !updateRotations(val2, val2Top, val2Bottom, *Ait, *Bit, val1Top))
-            return -1;
-    }
-    auto minRotations = std::numeric_limits<int>::max();
-    getMinRotations(minRotations, val1Top);
-    getMinRotations(minRotations, val1Bottom);
-    getMinRotations(minRotations, val2Top);
-    getMinRotations(minRotations, val2Bottom);
+    auto minRotationsTop = std::min(getMinRotationsTop(A, B), getMinRotationsTop(B, A));
+    auto minRotationsBottom = std::min(getMinRotationsBottom(A, B), getMinRotationsBottom(B, A));
+    auto minRotations = std::min(minRotationsTop, minRotationsBottom);
+    if (minRotations == std::numeric_limits<int>::max())
+        return -1;
     return minRotations;
 }
 
