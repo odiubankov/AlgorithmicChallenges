ac6cf59083659d383b23fd2d1cd618901ab4ff98
Oleksii Diubankov <Oleksii.Diubankov@bmw.de>
2020-03-23 08:22:04 +0100

    leetcode: longest palindromic substring. Expand around center

--- cpp/leetcode/Google/Interview/DP/5_LongestPalindromicSubstring.h
+++ cpp/leetcode/Google/Interview/DP/5_LongestPalindromicSubstring.h
@@ -37,4 +37,39 @@ string longestPalindrome(const string& s) {
     return longestPalindrome;
 }
 
+using StrItT = std::string::const_iterator;
+
+void expandAroundCenter(
+        const StrItT& leftIt, const StrItT& rightIt,
+        const StrItT& beginIt, const StrItT& endIt,
+        string& longestPalindrome) {
+    auto left = leftIt;
+    auto right = rightIt;
+    while (left != beginIt && (right + 1) != endIt && *(left - 1) == *(right + 1)) {
+        --left;
+        ++right;
+    }
+    if ((std::distance(left, right + 1)) > longestPalindrome.length()) {
+        longestPalindrome = std::string{left, right + 1};
+    }
+}
+
+string longestPalindromeCenterExpand(const string& s) {
+    if (s.empty())
+        return "";
+    string longestPalindrome{s.front()};
+    for (auto it = begin(s) + 1; it != end(s); ++it) {
+        //even cnt
+        auto leftIt = it - 1;
+        if ((it + 1) != end(s) && *(it - 1) == *(it + 1)) {
+            expandAroundCenter(it - 1, it + 1, begin(s), end(s), longestPalindrome);
+        }
+        //odd cnt
+        if (*(it - 1) == (*it)) {
+            expandAroundCenter(it - 1, it, begin(s), end(s), longestPalindrome);
+        }
+    }
+    return longestPalindrome;
+}
+
 #endif //ALGORITHMICCHALLENGES_5_LONGESTPALINDROMICSUBSTRING_H
