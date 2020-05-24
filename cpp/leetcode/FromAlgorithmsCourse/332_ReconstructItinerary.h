156566b5ae39f8550e2ca5467d7ae3a0a59f99cf
Diubankov Oleksii <q479074@europe.bmw.corp>
2020-05-24 08:19:08 +0200

    leetcode: itinerary euler path

---
+++ cpp/leetcode/FromAlgorithmsCourse/332_ReconstructItinerary.h
@@ -0,0 +1,45 @@
+#ifndef ALGORITHMICCHALLENGES_332_RECONSTRUCTITINERARY_H
+#define ALGORITHMICCHALLENGES_332_RECONSTRUCTITINERARY_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+#include <queue>
+
+using namespace std;
+
+using CitiesQueueT = priority_queue<string, vector<string>, greater<string>>;
+using ConnectionsT = unordered_map<string, CitiesQueueT>;
+using ItineraryT = vector<string>;
+
+void visitCity(
+        string city,
+        ItineraryT& itinerary,
+        ConnectionsT& connections) {
+    auto cityConnections = connections.find(city);
+    if (cityConnections != end(connections)) {
+        while (!cityConnections->second.empty()) {
+            auto nextCity = cityConnections->second.top();
+            cityConnections->second.pop();
+            visitCity(move(nextCity), itinerary, connections);
+        }
+    }
+    itinerary.push_back(move(city));
+}
+
+ItineraryT findItinerary(vector<vector<string>> const& tickets) {
+    ConnectionsT connections;
+    for (auto& ticket : tickets) {
+        connections[ticket.front()].push(ticket.back());
+    }
+
+    ItineraryT itinerary;
+    visitCity("JFK", itinerary, connections);
+    reverse(begin(itinerary), end(itinerary));
+    return itinerary;
+}
+
+#endif //ALGORITHMICCHALLENGES_332_RECONSTRUCTITINERARY_H
