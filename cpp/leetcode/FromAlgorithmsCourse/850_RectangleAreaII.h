672b154a29fc9775e25a7eac13f3c9756247db10
Diubankov Oleksii <q479074@europe.bmw.corp>
2021-02-24 21:25:41 +0100

    Rectangle area - implement line sweep algorithm

--- cpp/leetcode/FromAlgorithmsCourse/850_RectangleAreaII.h
+++ cpp/leetcode/FromAlgorithmsCourse/850_RectangleAreaII.h
@@ -1,114 +1,88 @@
 //https://leetcode.com/problems/rectangle-area-ii/
+// line sweep algorithm
 #ifndef ALGORITHMICCHALLENGES_850_RECTANGLEAREAII_H
 #define ALGORITHMICCHALLENGES_850_RECTANGLEAREAII_H
 
 #include <vector>
-#include <queue>
-#include <limits>
 #include <map>
-#include <set>
 
 using namespace std;
 
 using RectangleT = vector<int>;
-struct RectangleBeginComp {
-    bool operator()(RectangleT const* r1, RectangleT const* r2) const noexcept {
-        return (r1->front() > r2->front());
-    }
+
+enum class VerticalSide {
+    Left,
+    Right
 };
-using RectBeginQueueT = priority_queue<RectangleT const*, vector<RectangleT const*>, RectangleBeginComp>;
 
-struct RectangleEndComp {
-    bool operator()(RectangleT const* r1, RectangleT const* r2) const noexcept {
-        return (r1->at(2) > r2->at(2));
-    }
+enum class HorizontalSide {
+    Top,
+    Bottom
 };
-using RectEndQueueT = priority_queue<RectangleT const*, vector<RectangleT const*>, RectangleEndComp>;
 
-using IsBeginT = bool;
-using AreaHeightT = multiset<pair<int, IsBeginT>>;
-auto constexpr MODULO = 1000000007;//10^9 + 7
-using AreaT = unsigned long long;
-
-void addRect(
-        AreaHeightT& areaHeight,
-        RectBeginQueueT& rectBeginQueue,
-        RectEndQueueT& rectEndQueue) {
-    areaHeight.insert(make_pair(rectBeginQueue.top()->at(1), true));
-    areaHeight.insert(make_pair(rectBeginQueue.top()->at(3), false));
-    rectEndQueue.push(rectBeginQueue.top());
-    rectBeginQueue.pop();
-}
-
-void removeRect(
-        AreaHeightT& areaHeight,
-        RectEndQueueT& rectEndQueue) {
-    areaHeight.erase(areaHeight.find(make_pair(rectEndQueue.top()->at(1), true)));
-    areaHeight.erase(areaHeight.find(make_pair(rectEndQueue.top()->at(3), false)));
-    rectEndQueue.pop();
-}
-
-AreaT getHeight(AreaHeightT const& areaHeight) {
-    if (areaHeight.empty())
-        return 0;
-    AreaT totalHeight = 0;
-    AreaT heightBegin = 0;
-    int rectsCnt = 0;
-    for (auto& height : areaHeight) {
-        if (rectsCnt == 0) {
-            heightBegin = height.first;
-        }
-        if (height.second) {
-            ++rectsCnt;
-        } else {
-            --rectsCnt;
-        }
-
-        if (rectsCnt == 0) {
-            AreaT heightEnd = height.first;
-            totalHeight += (heightEnd - heightBegin);
-        }
-    }
-
-    return totalHeight;
-}
+struct RectEdge {
+    long long yBottom = 0;
+    long long yTop = 0;
+    VerticalSide side = VerticalSide::Left;
+};
 
 int rectangleArea(vector<RectangleT> const& rectangles) {
-    if (rectangles.empty())
-        return 0;
-    RectBeginQueueT rectBeginQueue;
+    multimap<long long, RectEdge> xPos;
     for (auto& rect : rectangles) {
-        rectBeginQueue.push(&rect);
-    }
-    RectEndQueueT rectEndQueue;
-    AreaHeightT areaHeight;
-    AreaT areaBegin = rectBeginQueue.top()->front();
-    while (!rectBeginQueue.empty() && areaBegin == rectBeginQueue.top()->front()) {
-        addRect(areaHeight, rectBeginQueue, rectEndQueue);
+        xPos.emplace(rect[0], RectEdge {rect[1], rect[3], VerticalSide::Left} );
+        xPos.emplace(rect[2], RectEdge {rect[1], rect[3], VerticalSide::Right});
     }
 
-    AreaT height = getHeight(areaHeight);
-    AreaT area = 0;
-    while (!rectBeginQueue.empty() || !rectEndQueue.empty()) {
-        AreaT areaEnd = numeric_limits<int>::max();
-        if (!rectBeginQueue.empty()) {
-            areaEnd = rectBeginQueue.top()->front();
+    long long totalArea = 0;
+    multimap<long long, HorizontalSide> yPos;
+    auto xIt = begin(xPos);
+    yPos.emplace(xIt->second.yBottom, HorizontalSide::Bottom);
+    yPos.emplace(xIt->second.yTop, HorizontalSide::Top);
+    long long prevX = xIt->first;
+    long long currentHeight = xIt->second.yTop - xIt->second.yBottom;
+    ++xIt;
+    for (;xIt != end(xPos); ++xIt) {
+        if (xIt->second.side == VerticalSide::Left) {
+            yPos.emplace(xIt->second.yBottom, HorizontalSide::Bottom);
+            yPos.emplace(xIt->second.yTop, HorizontalSide::Top);
+        } else {
+            auto yIt = yPos.find(xIt->second.yBottom);
+            while (yIt->second != HorizontalSide::Bottom)
+                ++yIt;
+            yPos.erase(yIt);
+            yIt = yPos.find(xIt->second.yTop);
+            while (yIt->second != HorizontalSide::Top)
+                ++yIt;
+            yPos.erase(yIt);
         }
-        if (!rectEndQueue.empty()) {
-            areaEnd = min<AreaT>(areaEnd, rectEndQueue.top()->at(2));
+
+        totalArea += currentHeight * (xIt->first - prevX);
+        totalArea %= 1000000007;
+        prevX = xIt->first;
+
+        currentHeight = 0;
+        if (!yPos.empty()) {
+            int segmentsCnt = 1;
+            auto yIt = begin(yPos);
+            long long yBegin = yIt->first;
+            ++yIt;
+            for (; yIt != end(yPos); ++yIt) {
+                if (yIt->second == HorizontalSide::Bottom) {
+                    if (segmentsCnt == 0) {
+                        yBegin = yIt->first;
+                    }
+                    ++segmentsCnt;
+                } else { // yIt->second == HorizontalSide::Top
+                    --segmentsCnt;
+                    if (segmentsCnt == 0) {
+                        currentHeight += (yIt->first - yBegin);
+                    }
+                }
+            }
         }
-        area += (areaEnd - areaBegin) * height;
-        area %= MODULO;
-        areaBegin = areaEnd;
-        while (!rectBeginQueue.empty() && areaBegin == rectBeginQueue.top()->front()) {
-            addRect(areaHeight, rectBeginQueue, rectEndQueue);
-        }
-        while (!rectEndQueue.empty() && areaBegin == rectEndQueue.top()->at(2)) {
-            removeRect(areaHeight, rectEndQueue);
-        }
-        height = getHeight(areaHeight);
     }
-    return area;
+
+    return totalArea;
 }
 
 #endif //ALGORITHMICCHALLENGES_850_RECTANGLEAREAII_H
--- cpp/leetcode/FromAlgorithmsCourse/850_RectangleAreaIITest.cpp
+++ cpp/leetcode/FromAlgorithmsCourse/850_RectangleAreaIITest.cpp
@@ -4,7 +4,3 @@
 TEST(RectangleArea, test) {
     ASSERT_EQ(6, rectangleArea({{0,0,2,2}, {1,0,2,3}, {1,0,3,1}}));
 }
-
-TEST(RectangleArea, test2) {
-    ASSERT_EQ(49, rectangleArea({{0,0,1000000000,1000000000}}));
-}
