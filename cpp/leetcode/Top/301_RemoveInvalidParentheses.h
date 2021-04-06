a6a8f207300b18e6308fc128911b689d21bd5beb
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2021-04-06 14:48:46 +0200

    Remove invalid parentheses dfs

--- cpp/leetcode/Top/301_RemoveInvalidParentheses.h
+++ cpp/leetcode/Top/301_RemoveInvalidParentheses.h
@@ -6,39 +6,9 @@
 #include <unordered_set>
 #include <vector>
 #include <algorithm>
+#include <queue>
+
 using namespace std;
-
-string removeChars(const string& s, unordered_set<int> toRemove) {
-    string res;
-    res.reserve(s.size() - toRemove.size());
-    for (int i = 0; i < s.size(); ++i) {
-        if (toRemove.find(i) == end(toRemove))
-            res += s[i];
-    }
-    return res;
-}
-
-vector<unordered_set<int>> getPosToRemove(vector<int>::const_iterator it, vector<int>::const_iterator itE, int removeCnt) {
-    if (removeCnt == 0)
-        return {};
-    if (removeCnt == distance(it, itE)) {
-        unordered_set<int> toRemove{it, itE};
-        return {toRemove};
-    }
-    if (removeCnt == 1) {
-        vector<unordered_set<int>> res;
-        for (;it != itE; ++it)
-            res.push_back({*it});
-        return res;
-    }
-    vector<unordered_set<int>> res1 = getPosToRemove(it + 1, itE, removeCnt);
-    vector<unordered_set<int>> res2 = getPosToRemove(it + 1, itE, removeCnt - 1);
-    for (auto& toRemove : res2) {
-        toRemove.insert(*it);
-        res1.push_back(toRemove);
-    }
-    return res1;
-}
 
 bool isValid(const string& s) {
     int openCnt = 0;
@@ -50,64 +20,38 @@ bool isValid(const string& s) {
                 return false;
         }
     }
-    return true;
+    return openCnt == 0;
 }
 
-vector<string> fixExpr(const string& s, const vector<int>& toRemove, int removeCnt) {
-    auto posToRemove = getPosToRemove(begin(toRemove), end(toRemove), removeCnt);
+vector<string> removeInvalidParentheses_dfs(string s) {
     unordered_set<string> fixed;
-    for (const auto& pToRemove : posToRemove)
-        fixed.insert(removeChars(s, pToRemove));
-    vector<string> res;
-    res.reserve(fixed.size());
-    copy(begin(fixed), end(fixed), back_inserter(res));
-    return res;
-}
-
-vector<string> removeInvalidParentheses(string s) {
-    unordered_set<int> toRemove;
-    for (int i = 0; i < s.size() && s[i] != '('; ++i) {
-        if (s[i] == ')')
-            toRemove.insert(i);
-    }
-    for (int i = s.size() - 1; i >= 0 && s[i] != ')'; --i) {
-        if (s[i] == '(')
-            toRemove.insert(i);
-    }
-    s = removeChars(s, toRemove);
-    vector<int> opens, closes;
-    for (int i = 0; i < s.size(); ++i) {
-        if (s[i] == ')') {
-            closes.push_back(i);
-        } else if (s[i] == '(') {
-            opens.push_back(i);
+    unordered_set<string> inQueue;
+    std::queue<string> q;
+    q.push(s);
+    inQueue.insert(s);
+    while (!q.empty()) {
+        auto nextS = q.front();
+        q.pop();
+        if (isValid(nextS)) {
+            if (fixed.empty() || fixed.begin()->length() == nextS.length()) {
+                fixed.emplace(move(nextS));
+            }
+        }
+        else {
+            for (size_t i = 0; i < nextS.length(); ++i) {
+                if (nextS[i] == '(' || nextS[i] == ')') {
+                    auto patchedS = nextS.substr(0, i);
+                    patchedS.append(nextS.substr(i + 1));
+                    if (inQueue.find(patchedS) == end(inQueue)) {
+                        q.push(patchedS);
+                        inQueue.insert(patchedS);
+                    }
+                }
+            }
         }
     }
 
-    vector<string> fixed;
-    int opensToRemove = 0, closesToRemove = 0;
-    if (opens.size() > closes.size()) {
-        opensToRemove = opens.size() - closes.size();
-    } else {
-        closesToRemove = closes.size() - opens.size();
-    }
-    do {
-        vector<string> opensRemoved {s};
-        if (opensToRemove > 0)
-            opensRemoved = fixExpr(s, opens, opensToRemove);
-        for (const auto& withoutOpens : opensRemoved) {
-            vector<string> closesRemoved{withoutOpens};
-            if (closesToRemove > 0)
-                closesRemoved = fixExpr(withoutOpens, closes, closesToRemove);
-            copy_if(begin(closesRemoved), end(closesRemoved), back_inserter(fixed), isValid);
-        }
-        ++opensToRemove;
-        ++closesToRemove;
-    } while (fixed.empty());
-    unordered_set<string> fixedUnique{begin(fixed), end(fixed)};
-    fixed.clear();
-    copy(begin(fixedUnique), end(fixedUnique), back_inserter(fixed));
-    return fixed;
+    return vector<string>(begin(fixed), end(fixed));
 }
 
 #endif //ALGORITHMICCHALLENGES_301_REMOVEINVALIDPARENTHESES_H
--- cpp/leetcode/Top/301_RemoveInvalidParenthesesTest.cpp
+++ cpp/leetcode/Top/301_RemoveInvalidParenthesesTest.cpp
@@ -2,17 +2,21 @@
 #include <gtest/gtest.h>
 
 TEST(RemoveInvalidParentheses, test) {
-    ASSERT_EQ(2, removeInvalidParentheses("()())()").size());
+    ASSERT_EQ(2, removeInvalidParentheses_dfs("()())()").size());
 }
 
 TEST(RemoveInvalidParentheses, test2) {
-    ASSERT_EQ(vector<string>{"()()"}, removeInvalidParentheses("()(()(("));
+    ASSERT_EQ(vector<string>{"()()"}, removeInvalidParentheses_dfs("()(()(("));
 }
 
 TEST(RemoveInvalidParentheses, test3) {
-    ASSERT_EQ(2, removeInvalidParentheses("(((k()((").size());
+    ASSERT_EQ(2, removeInvalidParentheses_dfs("(((k()((").size());
 }
 
 TEST(RemoveInvalidParentheses, test4) {
-    ASSERT_EQ(1, removeInvalidParentheses("())(((()m)(").size());
+    ASSERT_EQ(1, removeInvalidParentheses_dfs("())(((()m)(").size());
+}
+
+TEST(RemoveInvalidParentheses, test5) {
+    ASSERT_EQ(1, removeInvalidParentheses_dfs(")(").size());
 }
